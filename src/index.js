e7f330727224cfae7e37fa21efe43b4bc6c41176
Vương Đức Tuấn <lap11970@LAP11970s-MacBook-Pro-2.local>
2019-12-13 12:08:19 +0700

    push code

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { store, history} from './store';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+
+import App from './components/App';
+
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={App}/>
+                </Switch>
+            </BrowserRouter>
+        </ConnectedRouter>
+    </Provider>
+
+), document.getElementById('root'));
